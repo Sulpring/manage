9ab1d645f95a51234f3d79a9ec0742ae87ae34e9
cjs <jschae02@khu.ac.kr>
2024-12-04 16:06:31 +0900

    feat/product 리스트 템플릿 추가

--- manage_main/urls.py
+++ manage_main/urls.py
@@ -6,13 +6,16 @@ from manage_main.views.user_views import (
     register_showing_view, # 계정등록 템플릿 뷰
     register_process_view # 계정등록 처리 뷰
 )
-# from manage_main.views.product_view import 
+from manage_main.views.product_view import (
+    product_list_showing_view, # 프로덕트 리스트 뷰
+)
 
 urlpatterns = [
     path('', index_view, name='index'), # 초기화면
     path('login/', login_showing_view), # 로그인 화면
     path('login/process/', login_process_view), # 로그인 처리
     path('register/', register_showing_view), # 계정등록 화면
-    path('register/process/', register_process_view) # 계정등록 화면
+    path('register/process/', register_process_view), # 계정등록 화면
 
+    path('product/list/', product_list_showing_view), # 프로덕트 리스트 화면
 ]
--- manage_main/views/product_view.py
+++ manage_main/views/product_view.py
@@ -1,30 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from manage_main.models import User
 
-def user_list(request):
-    users = User.objects.all()
-    return render(request, 'user_product/user_list.html', {'users': users})
-
-def user_create(request):
-    if request.method == 'POST':
-        user_id = request.POST['id']
-        password = request.POST['password']
-        User.objects.create(id=user_id, password=password)
-        return redirect('user_list')
-    return render(request, 'user_product/user_form.html')
-
-def user_edit(request, pk):
-    user = get_object_or_404(User, pk=pk)
-    if request.method == 'POST':
-        user.id = request.POST['id']
-        user.password = request.POST['password']
-        user.save()
-        return redirect('user_list')
-    return render(request, 'user_product/user_form.html', {'user': user})
-
-def user_delete(request, pk):
-    user = get_object_or_404(User, pk=pk)
-    if request.method == 'POST':
-        user.delete()
-        return redirect('user_list')
-    return render(request, 'user_product/user_confirm_delete.html', {'user': user})
+def product_list_showing_view(request):
+    return render(request, 'products/product_list.html')
