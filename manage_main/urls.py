0359914e3c747d52ec5547bf2c1fe47397f12f83
cjs <jschae02@khu.ac.kr>
2024-12-04 15:02:21 +0900

    feat/초기화면 설정

--- manage_main/urls.py
+++ manage_main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from manage_main.views.user_views import (
+    index, # 초기화면
     login_view, # 로그인 뷰
+    
 )
 # from manage_main.views.product_view import 
 
 urlpatterns = [
-path('login/', login_view, name='login'),
+    path('/', index),
+    path('login/', login_view, name='login'),
 ]
