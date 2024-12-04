06ca3dc624c93580db56987506b8f43e349957ca
cjs <jschae02@khu.ac.kr>
2024-12-04 15:31:02 +0900

    fix/로그인 로직 추가

--- manage_main/views/user_views.py
+++ manage_main/views/user_views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+# 모델
+from manage_main.models import User
 
 # 초기화면(=로그인 화면) 보여주기
 def index_view(request):
@@ -9,12 +12,17 @@ def login_view(request):
     if request.method == 'POST':
         user_id = request.POST.get('id')
         password = request.POST.get('password')
-        print('아이디 비번')
-        print(user_id, " ", password)
-        # 로그인 처리 로직 (예: 사용자 인증)
-        # if user_id == "admin" and password == "password123":
-        #     return redirect('home')  # 인증 성공 시 홈으로 리다이렉트
-        # else:
-        #     return render(request, 'user_product/login.html', {'error': '아이디 또는 비밀번호가 잘못되었습니다.'})
-    return render(request, 'user_product/login.html')
+        # 데스트
+        print('아이디: ', user_id)
+        print("비번: ", password)
+
+        # 로그인 로직
+        user = User.objects.filter(id=user_id).first() or False # 없으면 False
+        if((user == False) or user.password != password): # 아이디가 없거나 비번이 잘못되면
+            response = {"error": '아이디 또는 비밀번호가 없거나 잘못되었습니다'}
+            return JsonResponse(response)
+        else:
+            # return redirect('home')  # 인증 성공 시 홈으로 리다이렉트
+            return JsonResponse({"message": "등록성공"})
+    return render(request, 'users/login.html')
 
