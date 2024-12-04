815f88974f3004c76941273fc3264d34a3ca5bc2
cjs <jschae02@khu.ac.kr>
2024-12-04 15:15:39 +0900

    feat/login뷰 임시  설정

--- manage_main/urls.py
+++ manage_main/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from manage_main.views.user_views import (
-    index, # 초기화면
+    index_view, # 초기화면
     login_view, # 로그인 뷰
     
 )
 # from manage_main.views.product_view import 
 
 urlpatterns = [
-    path('/', index),
+    path('', index_view, name='index'),
     path('login/', login_view, name='login'),
 ]
--- manage_main/views/user_views.py
+++ manage_main/views/user_views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 
 # 초기화면(=로그인 화면) 보여주기
-def index(request):
-    return render(request, 'user_product/login.html')
+def index_view(request):
+    return render(request, 'users/login.html')
 
 # 로그인 로직
 def login_view(request):
     if request.method == 'POST':
         user_id = request.POST.get('id')
         password = request.POST.get('password')
-
-        # 여기에 로그인 로직
-        # 그냥 user_id로 찾고, password가 일치하면 확인
-        print("로그인 완료에요")
-        return 'hi'
-
-        # return redirect('home')  # 로그인 성공 시 홈으로 리다이렉트
+        print('아이디 비번')
+        print(user_id, " ", password)
+        # 로그인 처리 로직 (예: 사용자 인증)
+        # if user_id == "admin" and password == "password123":
+        #     return redirect('home')  # 인증 성공 시 홈으로 리다이렉트
+        # else:
+        #     return render(request, 'user_product/login.html', {'error': '아이디 또는 비밀번호가 잘못되었습니다.'})
     return render(request, 'user_product/login.html')
+
